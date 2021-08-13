929f9683d19be891e564bce63218004114a234fe
Víctor Hidalgo <victor.hidalgo.lorenzo@gmail.com>
2021-08-13 12:18:29 +0200

    Add dependency updates plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 buildscript {
     dependencies {
-        val libs = project.extensions.getByType<VersionCatalogsExtension>().named("libs") as org.gradle.accessors.dm.LibrariesForLibs
+        val libs = project.extensions.getByType<VersionCatalogsExtension>()
+            .named("libs") as org.gradle.accessors.dm.LibrariesForLibs
         classpath(libs.gradlePlugins.android)
         classpath(libs.gradlePlugins.kotlin)
+        classpath(libs.gradlePlugins.versionsBenManes)
     }
 
     repositories {
@@ -11,6 +13,27 @@ buildscript {
             url = uri("https://plugins.gradle.org/m2/")
         }
     }
+}
+
+apply(plugin = "com.github.ben-manes.versions")
+
+fun String.isNonStable(): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(this)
+    return isStable.not()
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+
+    rejectVersionIf {
+        candidate.version.isNonStable()
+    }
+
+    checkForGradleUpdate = true
+    outputFormatter = "json"
+    outputDir = "build/dependencyUpdates"
+    reportfileName = "report"
 }
 
 allprojects {
