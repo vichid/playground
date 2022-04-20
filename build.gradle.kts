b4053c050a08aae6ebe2fd09a2e3cfd6aafd77b0
Víctor Hidalgo <victor.hidalgo.lorenzo@gmail.com>
2022-04-20 14:26:43 +0200

    Move logic to plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,28 +20,9 @@ plugins {
 }
 apply(plugin = "android-build-logic")
 
-allprojects {
-    buildscript {
-        repositories {
-            google()
-            maven { url = uri("https://plugins.gradle.org/m2/") }
-        }
-    }
-
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            allWarningsAsErrors = true
-            freeCompilerArgs = listOf(
-                "-Xopt-in=kotlin.RequiresOptIn",
-            )
-        }
-    }
+subprojects {
     repositories {
         google()
         mavenCentral()
     }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
 }
