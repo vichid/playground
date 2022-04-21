85c24d4eda717039ddc31812455789ff3d646239
Víctor Hidalgo <victor.hidalgo.lorenzo@gmail.com>
2022-04-21 15:57:37 +0200

    Add benchmarks

---
+++ macrobenchmark/src/main/java/com/playground/macrobenchmark/StartupBenchmark.kt
@@ -0,0 +1,67 @@
+package com.playground.macrobenchmark
+
+import androidx.benchmark.macro.BaselineProfileMode
+import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Run this benchmark from Studio to see startup measurements, and captured system traces
+ * for investigating your app's performance from a cold state.
+ */
+class ColdStartupBenchmark : AbstractStartupBenchmark(StartupMode.COLD)
+
+/**
+ * Run this benchmark from Studio to see startup measurements, and captured system traces
+ * for investigating your app's performance from a warm state.
+ */
+class WarmStartupBenchmark : AbstractStartupBenchmark(StartupMode.WARM)
+
+/**
+ * Run this benchmark from Studio to see startup measurements, and captured system traces
+ * for investigating your app's performance from a hot state.
+ */
+class HotStartupBenchmark : AbstractStartupBenchmark(StartupMode.HOT)
+
+/**
+ * Base class for benchmarks with different startup modes.
+ * Enables app startups from various states of baseline profile or [CompilationMode]s.
+ */
+abstract class AbstractStartupBenchmark(private val startupMode: StartupMode) {
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
+
+    @Test
+    fun startupNoCompilation() = startup(CompilationMode.None())
+
+    @Test
+    fun startupPartialCompilation() = startup(
+        CompilationMode.Partial(
+            baselineProfileMode = BaselineProfileMode.Disable,
+            warmupIterations = 1
+        )
+    )
+
+    @Test
+    fun startupPartialWithBaselineProfiles() =
+        startup(CompilationMode.Partial(baselineProfileMode = BaselineProfileMode.Require))
+
+    @Test
+    fun startupFullCompilation() = startup(CompilationMode.Full())
+
+    private fun startup(compilationMode: CompilationMode) = benchmarkRule.measureRepeated(
+        packageName = "com.example.playground",
+        metrics = listOf(StartupTimingMetric()),
+        compilationMode = compilationMode,
+        iterations = 10,
+        startupMode = startupMode,
+        setupBlock = {
+            pressHome()
+        }
+    ) {
+        startActivityAndWait()
+    }
+}
