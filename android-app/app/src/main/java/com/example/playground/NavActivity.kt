f10aad9abf64e10b9c60bc6a54c7c27ef821b4ce
Victor Hidalgo <victor.hidalgo.lorenzo@gmail.com>
2022-06-05 21:49:09 +0200

    Use coroutine scope

--- android-app/app/src/main/java/com/example/playground/NavActivity.kt
+++ android-app/app/src/main/java/com/example/playground/NavActivity.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.LaunchedEffect
-import androidx.lifecycle.lifecycleScope
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.core.di.ComponentHolder
@@ -12,6 +13,7 @@ import com.example.launch.impl.LaunchComponent
 import com.example.navigation.api.NavigatorEvent
 import com.example.navigation.impl.NavigationComponent
 import com.example.uicompose.theme.AppTheme
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
@@ -26,30 +28,36 @@ class NavActivity : ComponentActivity() {
         setContent {
             AppTheme {
                 val navController = rememberNavController()
-
-                LaunchedEffect(Unit) {
-                    navigationComponent.navigatorFactory()
-                        .destinations
-                        .onEach { navigatorEvent ->
-                            when (navigatorEvent) {
-                                is NavigatorEvent.Directions ->
-                                    navController.navigate(navigatorEvent.destination.route())
-                                NavigatorEvent.NavigateUp -> navController.navigateUp()
-                            }
-                        }
-                        .launchIn(lifecycleScope)
-                }
+                val coroutineScope = rememberCoroutineScope()
+                LaunchedEffect(Unit, observeNavigationEvents(navController, coroutineScope))
 
                 NavHost(
                     navController = navController,
                     startDestination = launchComponent.launchRouteFactory().provide(),
                     builder = {
-                        navigationComponent.composeNavigationFactorySet().forEach { factory ->
-                            factory.create(this)
-                        }
+                        navigationComponent.composeNavigationFactorySet()
+                            .forEach { factory ->
+                                factory.create(this)
+                            }
                     }
                 )
             }
         }
+    }
+
+    private fun observeNavigationEvents(
+        navController: NavHostController,
+        coroutineScope: CoroutineScope
+    ): CoroutineScope.() -> Unit = {
+        navigationComponent.navigatorFactory()
+            .destinations
+            .onEach { navigatorEvent ->
+                when (navigatorEvent) {
+                    is NavigatorEvent.Directions ->
+                        navController.navigate(navigatorEvent.destination.route())
+                    NavigatorEvent.NavigateUp -> navController.navigateUp()
+                }
+            }
+            .launchIn(coroutineScope)
     }
 }
